48caf3a1ac98164fc66e958a4c348bb982d42e06
bayorwor <bayorwor60@gmail.com>
2022-03-29 19:11:57 +0100

    first push

--- .history/src/index_20220329190044.js
+++ .history/src/index_20220329190044.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
-import "./styles/index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
