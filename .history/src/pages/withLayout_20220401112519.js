7907ff709a13f539c2099873824157561fef55de
Bayorwor <bayorwor60@gmail.com>
2022-04-01 11:30:21 +0000

    added blog posts

---
+++ .history/src/pages/withLayout_20220401112519.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+function withLayout(Component) {
+  function Layout() {
+    return (
+      <div className="layout">
+        <header className="appbar">
+          <Link to="/">
+            <span>Logo</span>
+          </Link>
+          <div>
+            <nav>
+              <span className="nav-link">
+                <Link to="/">Home</Link>
+              </span>
+              <span className="nav-link">
+                <Link to="/about">About</Link>
+              </span>
+              <span className="nav-link">
+                <Link to="/blog">Blog</Link>
+              </span>
+            </nav>
+          </div>
+        </header>
+        <main className="main-component">
+          <Component />
+        </main>
+        <footer>&copy; 2022 PreMEST</footer>
+      </div>
+    );
+  }
+
+  return Layout;
+}
+
+export default withLayout;
